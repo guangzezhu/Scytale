c7e26171155a169bff2eaabde8408c89ca11c76a
Gino Zhu <gino.zhu@ziyi.io>
2022-06-02 17:28:00 +0800

    update

---
+++ src/main/java/SimpleFrameTest.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author Gino Zhu
+ */
+public class SimpleFrameTest {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            SimpleFrame frame = new SimpleFrame();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
+    }
+}
+
+class SimpleFrame extends JFrame {
+    private  static final int DEFAULT_WIDTH = 300;
+    private  static final int DEFAULT_HEIGHT = 200;
+
+    public SimpleFrame() {
+        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+}
