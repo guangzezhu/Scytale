91f39123c12faf0a2318b5b07eeeef6882b46873
Gino Zhu <gino.zhu@ziyi.io>
2022-06-17 18:35:17 +0800

    update

---
+++ src/main/java/chapter_12/RadioButtonFrame.java
@@ -0,0 +1,38 @@
+package chapter_12;
+
+import javafx.scene.control.RadioButton;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class RadioButtonFrame extends JFrame {
+
+    private JPanel buttonPanel;
+    private ButtonGroup buttonGroup;
+    private JLabel label;
+    private static final int DEFAULT_SIZE = 36;
+
+    public RadioButtonFrame() {
+
+        label = new JLabel("To be or not to be, this is a question...");
+        label.setFont(new Font("Serif", Font.PLAIN, DEFAULT_SIZE));
+        add(label, BorderLayout.CENTER);
+
+        buttonPanel = new JPanel();
+        buttonGroup = new ButtonGroup();
+
+        addRadioButton("Small", 18);
+
+        add(buttonPanel, BorderLayout.SOUTH);
+        pack();
+    }
+
+    private void addRadioButton(String name, int size) {
+        boolean isSelected = size == DEFAULT_SIZE;
+        JRadioButton radioButton = new JRadioButton(name, isSelected);
+        radioButton.addActionListener(event -> {
+            label.setFont(new Font("Serif", Font.PLAIN, size));
+        });
+        buttonGroup.add(radioButton);
+    }
+}
